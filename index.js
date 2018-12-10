d40af02179f55ac5c8629e5c7c92545f5849323e
Nelson Dominguez <nlsn_dominguez@yahoo.com>
2018-12-10 13:30:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+var a = 1 
+var b = 2 
+
+function add(a, b) {
+  return a + b 
+}
+
+function multiply(a, b) {
+  return a * b 
+}
+
+function divide(a, b) {
+  return a / b 
+}
+
+function subtract(a, b) {
+  return a - b 
+}
+
+function inc(a){
+  a++;
+  return a
+}
+
+function dec(b){
+  b--;
+  return b 
+}
+
+function makeInt(a) {
+  return parseInt(a , 10) ===b;
+}
+
