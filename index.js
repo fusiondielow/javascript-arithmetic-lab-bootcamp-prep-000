ee57b71fd116d4f73463617776834ab2aa6ed740
Nelson Dominguez <nlsn_dominguez@yahoo.com>
2018-12-10 05:00:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,40 @@
+var a = 1
+var b = 2
+
+function add(a, b) {
+  return a + b 
+}
+
+function subtract(a, b) {
+  return a - b 
+}
+
+function multiply(a, b){
+  return a * b 
+}
+
+function divide(a, b){
+  return a / b 
+}
+
+var n = 5
+
+function inc(n){
+  n++;
+  return n
+}
+
+function dec(n){
+  n--;
+  return n
+}
+
+function makeInt(n) {
+  n += 10
+  return n
+}
+
+
+
+
+
